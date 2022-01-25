ee2288ebbafc487407f910e88e1d9b258e1a44c2
MeQHunter <fahmiadicandra9@gmail.com>
2022-01-25 09:06:57 +0700

    upload modul 2

--- routes/web.php
+++ routes/web.php
@@ -14,11 +14,15 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('Home');
+    return view('Home', [
+         "title" => "Home"
+    ]);     
+
 });
 
 Route::get('/about', function () {
     return view('About', [
+        "title" => "About",
         "nama" => "Fahmi Adicandra",
         "email" => "fahmiadicandraa@gmail.com",
         "gambar" => "IMG_9498.jpg"
@@ -26,5 +30,7 @@ Route::get('/about', function () {
 });
 
 Route::get('/gallery', function () {
-    return view('Gallery');
+    return view('Gallery', [
+    "title" => "Gallery"
+    ]);
 });
